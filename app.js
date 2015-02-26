b6ef8376323a135cee4bd024a3e3c4fda1613568
Jerry Chang <therealjerrycan@gmail.com>
2015-02-26 13:56:52 -0800

    intial UI design

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var http = require
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+
+//set to run on port 3000
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -37,6 +43,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    app.use(logger('dev'));
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -56,5 +63,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
+app.listen(3000);
 module.exports = app;
