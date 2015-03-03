9d7b975c8bcc7a3a2e501038b4196463addb13bc
Jerry Chang <therealjerrycan@gmail.com>
2015-03-03 12:31:02 -0800

    seperated javscript animations to public folder

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ public/javascripts/animations.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+  $('.img-circle').on('click', function(){
+    $(this).toggleClass('current')
+  });
+  console.log("hi")
+});
+
+  
