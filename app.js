fb278f69af03e1c2117f47ba72b906d73ea3ff5f
killerwails <iamthe.angrytoast@gmail.com>
2015-03-06 14:44:02 -0800

    folder rename

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
-var spawn = require('child_process').spawn;
 var twitter = require('./lib/twitter-api.js');
+var spawn = require('child_process').spawn;
 
 var IMAGE_FILE_PATH = './camera_images/image_stream.jpg'
-  
+
 var app = express();
 var server = require('http').Server(app);
 
@@ -22,8 +22,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'camera_images')));
 app.use(express.static(path.join(__dirname,'bower_components')));
- 
 
 var sockets = {};
 var proc;
@@ -49,16 +49,6 @@ io.on('connection', function(socket) {
     startStreaming(io);
   });
 
-  io.on('take-picture',function() {
-    fs.open(IMAGE_FILE_PATH, 'r', function(err,reader){
-      fs.open("./camera_images/image_capture.jpg",'w+',function(err,writer){
-        console.log(err)
-        fs.write(writer, reader.toBuffer, function(err,fd){
-          console.log('matt: ', err)
-        });
-      });
-    });
-  });
 });
 
 //if capturing already happening,will not re-init.Emits the last saved image
@@ -68,7 +58,7 @@ function stopStreaming() {
     if (proc) proc.kill();
     fs.unwatchFile(IMAGE_FILE_PATH);
   }
-}
+};
  //if capturing not started, start a new child process and then spawn it.Registers a watch on the changing file. Whenever the file changes we emit a URL to all connected clients.
 function startStreaming(io) {
  //_t param to avoid caching on the image
@@ -77,7 +67,7 @@ function startStreaming(io) {
     return;
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", IMAGE_FILE_PATH, "-t", "999999999", "-tl", "100"];
+  var args = ["-f","-w", "640", "-h", "480", "-o", "->", IMAGE_FILE_PATH];
   proc = spawn('raspistill', args);
  
   console.log('Watching for changes...');
